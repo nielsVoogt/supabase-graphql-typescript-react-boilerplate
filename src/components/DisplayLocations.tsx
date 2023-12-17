d6b9b6c1a42872f9af9397ec3b96ea58fefb264c
niels voogt <n.voogt@gmail.com>
2023-12-17 17:09:37 +0100

    Tempdump

--- src/components/DisplayLocations.tsx
+++ src/components/DisplayLocations.tsx
@@ -19,7 +19,14 @@ const DisplayLocations = () => {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
 
-  return data.locations.map(({ id, name, description, photo }) => (
+  type Location = {
+    id: number;
+    name: string;
+    description: string;
+    photo: string;
+  };
+
+  return data.locations.map(({ id, name, description, photo }: Location) => (
     <div key={id}>
       <h3>{name}</h3>
       <img width="400" height="250" alt="location-reference" src={`${photo}`} />
